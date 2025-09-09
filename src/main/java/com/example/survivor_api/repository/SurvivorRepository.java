ddfef8d46747fcee85d9e92f2d11f08c752c96f0
Sharen Rajenthiran <sharenrajenthiran@gmail.com>
2025-09-09 19:49:56 +0800

    Update SurvivorRepository (use MongoDB)

--- src/main/java/com/example/survivor_api/repository/SurvivorRepository.java
+++ src/main/java/com/example/survivor_api/repository/SurvivorRepository.java
@@ -1,4 +1,8 @@
 package com.example.survivor_api.repository;
 
-public class SurvivorRepository {
+import com.example.survivor_api.models.Survivor;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface SurvivorRepository extends MongoRepository<Survivor, String> {
+    long countByInfected(boolean infected);
 }
